628562c7512f9f2e9b978a3b061cb8fe4541bae9
TristanAtze <ben.sowieja@4sellers.de>
2025-01-24 11:55:51 +0100

    Update

--- HC4U 2.0/Program.cs
+++ HC4U 2.0/Program.cs
@@ -1,18 +1,65 @@
 using ChatApp;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
 
 namespace HC4U_2._0
 {
     internal static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            string repoUrl = "https://github.com/TristanAtze/HC4Uv2.git";
+            string repoDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "HC4Uv2");
+
+            if (Directory.Exists(repoDir))
+            {
+                UpdateRepo(repoDir);
+            }
+            else
+            {
+                CloneRepo(repoUrl, repoDir);
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new ChatForm());
+        }
+
+        private static void CloneRepo(string repoUrl, string repoDir)
+        {
+            ExecuteGitCommand($"clone {repoUrl} {repoDir}");
+        }
+
+        private static void UpdateRepo(string repoDir)
+        {
+            ExecuteGitCommand($"-C \"{repoDir}\" pull");
+        }
+
+        private static void ExecuteGitCommand(string arguments)
+        {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo
+            {
+                FileName = "git",
+                Arguments = arguments,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process process = Process.Start(processStartInfo))
+            {
+                using (StreamReader reader = process.StandardOutput)
+                {
+                    string output = reader.ReadToEnd();
+                    Console.WriteLine(output); 
+                }
+            }
         }
     }
 }
